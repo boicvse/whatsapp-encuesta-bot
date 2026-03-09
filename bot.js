a85fb0cfdab3ba71969af59cc32b46f56b6707dc
boicvse <car.caceres.fer@gmail.com>
2026-03-09 11:48:19 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,37 @@
 const wppconnect = require('@wppconnect-team/wppconnect');
 const cron = require('node-cron');
 
-wppconnect.create().then((client) => start(client));
+wppconnect.create({
+  session: 'encuesta-bot',
+  puppeteerOptions: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage'
+    ]
+  }
+})
+.then((client) => start(client))
+.catch((error) => console.error(error));
 
 function start(client) {
 
-  cron.schedule('0 19 * * *', () => {
+  // horario de Chile: 19:00 todos los días
+  cron.schedule('0 19 * * *', async () => {
 
     const grupo = 'ID_DEL_GRUPO@g.us';
 
-    client.sendPollMessage(grupo, {
+    await client.sendPollMessage(grupo, {
       name: '¿Asistirás mañana al trabajo?',
       options: [
         { name: 'Sí' },
         { name: 'No' },
-        { name: 'Llegaré tarde, yo marco' }
+        { name: 'Llegaré tarde - Marco yo' }
       ]
     });
+
+    console.log('Encuesta enviada');
 
   });
 
